690bb32b18d8b450d04e5b18011968f11852075e
oldlonecoder <lussier.serge@gmail.com>
2025-03-07 22:16:24 -0500

    (en/US): Add missing subdir! (fr/CA): Ajout du sous-rep. manquant! --------------------------------------- nouveau fichier : bitstest.app/main.cc

---
+++ bitstest.app/main.cc
@@ -0,0 +1,60 @@
+#include <tuxnbits++/bits/diagnostic.h>
+
+using dlog = diagnostic;
+namespace tux
+{
+
+
+class test
+{
+    //io::terminal term{nullptr,"tests",{}};
+    tux::string::view_list _args{};
+public:
+    test(const std::string& tname, tux::string::view_list&& vargs);
+    ~test(){ _args.clear(); }
+
+    void terminate();
+    //est::alu expr();
+    dlog::file::handle h{};
+    rem::cc run();
+
+};
+
+
+
+
+rem::cc test::run()
+{
+    h = *dlog::new_file("tests");
+    auto l = dlog::message(h) << color::r << " - The Great Beginning of the tux::bitsnbytes++ !\n";
+    l << color::yellow << "More to come!" << l;
+    dlog::Test Testest("Testing the diangostic::Test object...");
+    auto r = Testest.exec<std::string>("auto", [&](diagnostic::Test& Tst)->std::string {
+        return "allo";
+    });
+    terminate();
+    return rem::cc::ok;
+}
+
+
+
+test::test(const std::string &tname, string::view_list &&vargs): _args(std::move(vargs)){}
+
+
+void test::terminate()
+{
+    dlog::close(h);
+}
+
+
+}
+
+
+
+auto main(int argc, char** argv, char** env) -> int
+{
+    std::cout << "Hello, World\n";
+    tux::test tests("obb tests", tux::string::string_view_list(argc,argv,1));
+    return static_cast<int>(tests.run());
+}
+
