11ea45f5ac193f9979df38e28046b6828275aac0
oldlonecoder <lussier.serge@gmail.com>
2025-03-07 22:13:23 -0500

    (en/us): Experimenting a diagnostic::Test approach (fr/ca): Experimente une forme de test sous diagnostic::Test -------------------------------------------------------------- 	modifié : CMakeLists.txt 	modifié : tuxnbits++/bits/diagnostic.cc 	modifié : tuxnbits++/bits/diagnostic.h 	modifié : tuxnbits++/bits/diagnostic.inc 	modifié : tuxnbits++/bits/expect.h 	nouveau fichiers : tuxnbits++/bits/expect.inc, 	bitstest.cmake

---
+++ bitstest.cmake
@@ -0,0 +1,25 @@
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+message("Dir: " ${CMAKE_CURRENT_SOURCE_DIR})
+
+add_executable(bitstest.app
+    bitstest.app/main.cc
+)
+
+
+target_include_directories(bitstest.app PUBLIC
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>"
+)
+
+message("Build dir : build/")
+
+target_link_directories(bitstest.app PUBLIC build)
+add_dependencies(bitstest.app tuxnbits)
+
+
+# TABARNAK! j'joubli toujours de linker les librairies dans le bon ordre!!!!!
+target_link_libraries(bitstest.app
+    tuxnbits++.bits
+)
+
